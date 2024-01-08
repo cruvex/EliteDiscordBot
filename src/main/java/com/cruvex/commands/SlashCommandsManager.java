362d3742a39d3fc090032eaa17e1826df1114ac3
cruvex <woma811@gmail.com>
2024-01-08 19:16:01 +0100

    Add event listener for managing ButtonInteractions

--- src/main/java/com/cruvex/commands/SlashCommandsManager.java
+++ src/main/java/com/cruvex/commands/SlashCommandsManager.java
@@ -2,11 +2,15 @@ package com.cruvex.commands;
 
 import com.cruvex.EliteDiscordBot;
 import com.cruvex.util.EmbedUtil;
+import com.cruvex.util.Util;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction;
 import org.slf4j.Logger;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 public class SlashCommandsManager extends ListenerAdapter {
@@ -48,6 +52,29 @@ public class SlashCommandsManager extends ListenerAdapter {
                 log("############ END ############");
             }
         }
+    }
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+        ButtonInteraction buttonInteraction = event.getInteraction();
+        log("############ ButtonInteractionEvent ############");
+        log("New ButtonInteractionEvent registered " + buttonInteraction.getComponentId() + " | " + event.getTimeCreated());
+        COMMAND_MAP.forEach((_ignored, command) -> {
+            if (Util.isEmptyOrNull(command.getButtonInteractionsMap()))
+                return;
+
+            command.getButtonInteractionsMap().forEach((buttonId, method) -> {
+                if (buttonId.equals(buttonInteraction.getComponentId())) {
+                    log("Method found for button '" + buttonId + "': " + command.getClass() + " - " + method.getName());
+                    try {
+                        method.invoke(command, buttonInteraction);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        });
+        log("############ END ############");
     }
 
     private void log(Object message) {
