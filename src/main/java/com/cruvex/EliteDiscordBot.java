79f1d7f6e506c2538a36ca634206e359f75a6a83
cruvex <woma811@gmail.com>
2024-01-18 02:00:23 +0100

    Defined static logger functions + defined primaryColor + cleanup some things

--- src/main/java/com/cruvex/EliteDiscordBot.java
+++ src/main/java/com/cruvex/EliteDiscordBot.java
@@ -3,11 +3,14 @@ package com.cruvex;
 import com.cruvex.commands.AbstractCommand;
 import com.cruvex.commands.SlashCommandsManager;
 import com.cruvex.commands.call.CallLeaderboardCommand;
+import com.cruvex.commands.call.CallLogParserCommand;
 import com.cruvex.commands.call.CallsCommand;
 import com.cruvex.commands.general.PingCommand;
+import com.cruvex.commands.message.MessageParserCommand;
 import com.cruvex.config.Config;
 import com.cruvex.eventlisteners.GuildEvents;
 import com.cruvex.eventlisteners.GuildVoiceJoinLeaveEvents;
+import com.cruvex.util.Util;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.Getter;
@@ -24,9 +27,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
+import java.awt.*;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EliteDiscordBot {
@@ -45,6 +50,8 @@ public class EliteDiscordBot {
     public static final @Getter Map<String, AbstractCommand> commandMap = new HashMap<>();
 
     private static final @Getter ArrayList<ListenerAdapter> eventListeners = new ArrayList<>();
+
+    public static final Color primaryColor = Color.decode("#e2be43");
 
     static {
         logger = LoggerFactory.getLogger(EliteDiscordBot.class);
@@ -94,9 +101,11 @@ public class EliteDiscordBot {
     private void registerCommands() {
         getCommandMap().put("ping", new PingCommand());
         // Command only needed to be used once to parse #no-mic and insert all events into database
-        //getCommandMap().put("parse-logs", new CallLogParserCommand());
+        //getCommandMap().put("parse-call-logs", new CallLogParserCommand());
         getCommandMap().put("calls", new CallsCommand());
         getCommandMap().put("call-lb", new CallLeaderboardCommand());
+        // Command for setting base for amount of messages sent per user - work in progress
+        //getCommandMap().put("parse-messages", new MessageParserCommand());
     }
 
     private void registerEventListeners() {
@@ -106,11 +115,7 @@ public class EliteDiscordBot {
     }
 
     private static boolean isRunningInIntelliJ(String[] args) {
-        for (String argument : args) {
-            if (argument.equals("-IntelliJ"))
-                return true;
-        }
-        return false;
+        return new ArrayList<>(List.of(args)).contains("-IntelliJ");
     }
 
     /*
@@ -140,8 +145,10 @@ public class EliteDiscordBot {
         Connection connection = null;
         try {
             if (dataSource != null) {
+                long start = System.currentTimeMillis();
                 connection = dataSource.getConnection();
-                logger.info("[DB-CONNECTION] Successfully got connection from pool");
+                long duration = System.currentTimeMillis() - start;
+                logger.info("[DB-CONNECTION] Successfully got connection from pool - " + Util.formatTime(duration));
             } else {
                 logger.error("[DB-CONNECTION] Error getting connection from pool - dataSource is null");
             }
@@ -151,9 +158,19 @@ public class EliteDiscordBot {
         return connection;
     }
 
-    public static void log(Object logMessage) {
+    public static void info(Object logMessage) {
         String className = Thread.currentThread().getStackTrace()[2].getClassName();
         logger.info("[" + className.substring(className.lastIndexOf('.') + 1) + "] " + logMessage);
+    }
+
+    public static void warn(Object logMessage) {
+        String className = Thread.currentThread().getStackTrace()[2].getClassName();
+        logger.warn("[" + className.substring(className.lastIndexOf('.') + 1) + "] " + logMessage);
+    }
+
+    public static void error(Object logMessage) {
+        String className = Thread.currentThread().getStackTrace()[2].getClassName();
+        logger.error("[" + className.substring(className.lastIndexOf('.') + 1) + "] " + logMessage);
     }
 
     public static void logSQL(Object logMessage) {
